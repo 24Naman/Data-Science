6eaa25590902b387fb68c24fd9b999dc03e86072
Naman Jain <24namanjain@gmail.com>
2017-09-22 01:57:40 +0530

    This is second commit

--- Types Of Data/GeoMean.py
+++ Types Of Data/GeoMean.py
@@ -4,10 +4,13 @@
     Geometric Mean
 
 """
-
-import numpy as np
+from functools import reduce
 
 # data of temperature
 data = [31, 32, 32, 31, 28, 29, 31, 38, 32, 31, 30, 29, 30, 31, 26]
 
+product = reduce(lambda x, y: x * y, data)
 
+geo_mean = product ** (1 / len(data))
+
+print("Geo Mean = ", geo_mean)
