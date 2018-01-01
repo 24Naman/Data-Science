ba605a35d01e39328b94832a0713e99d925bd998
Naman Jain <24namanjain@gmail.com>
2018-01-01 23:10:11 +0530

    Added docstring and comments

--- Types Of Data/World_alcohol_data.py
+++ Types Of Data/World_alcohol_data.py
@@ -2,10 +2,20 @@
 
 import pandas as pd
 
+"""
+    World alcohol data
+       
+"""
+
 LINK = "https://raw.githubusercontent.com/sinanuozdemir/principles_of_data_science" \
        "/master/data/chapter_2/drinks.csv"
 
 DRINKS = pd.read_csv(LINK)
-print(DRINKS.head())  # printing first 5 rows of csv
-print("Continent description", DRINKS['continent'].describe())  # qualitative column
-print("Beer servings description", DRINKS['beer_servings'].describe())  # quantitative column
+# printing first 5 rows of csv
+print(DRINKS.head())
+
+# printing first 5 rows of csv
+print("Continent description", DRINKS['continent'].describe())
+
+# quantitative column
+print("Beer servings description", DRINKS['beer_servings'].describe())
