c5212473e2fdb00b922d04ee6ced121d8758a972
Naman Jain <24namanjain@gmail.com>
2018-04-26 20:59:23 +0530

    Added docstring and comments

--- The Five Steps Of Data Science/Titanic.py
+++ The Five Steps Of Data Science/Titanic.py
@@ -8,24 +8,36 @@
 import numpy as np
 import pandas as pd
 
-titanic = pd.read_csv(r"../datasets/csv/Stat2Data/Titanic.csv")
-print(f"First five records: {titanic.head()}")
-print(f"Shape of dataset: {titanic.shape}")
 
-# replacing female with 1 and male with 0 in sex column
-# to convert a qualitative column into a numerical column
-titanic['Sex'] = np.where(titanic['Sex'] == 'female', [1], [0])
+def main():
+    """
+    Main function
+    :return:
+    :rtype:
+    """
 
-print("Number of null values: ", titanic.isnull().sum(), sep='\n')
+    titanic = pd.read_csv(r"../datasets/csv/Stat2Data/Titanic.csv")
+    print(f"First five records: {titanic.head()}")
+    print(f"Shape of dataset: {titanic.shape}")
 
-# calculating mean age
-average_age = titanic['Age'].mean()
+    # replacing female with 1 and male with 0 in sex column
+    # to convert a qualitative column into a numerical column
+    titanic['Sex'] = np.where(titanic['Sex'] == 'female', [1], [0])
 
-# replacing null values with average age
-titanic['Age'].fillna(average_age, inplace=True)
+    print("Number of null values: ", titanic.isnull().sum(), sep='\n')
 
-print("Number of null values ->", titanic.isnull().sum(), sep='\n')
+    # calculating mean age
+    average_age = titanic['Age'].mean()
 
-# average age by gender
-avg_age_by_gender = titanic.groupby('Sex')['Age'].mean()
-print(f"Average age of Female: {avg_age_by_gender[0]} and Male: {avg_age_by_gender[1]}")
+    # replacing null values with average age
+    titanic['Age'].fillna(average_age, inplace=True)
+
+    print("Number of null values ->", titanic.isnull().sum(), sep='\n')
+
+    # average age by gender
+    avg_age_by_gender = titanic.groupby('Sex')['Age'].mean()
+    print(f"Average age of Female: {avg_age_by_gender[0]} and Male: {avg_age_by_gender[1]}")
+
+
+if __name__ == '__main__':
+    main()
