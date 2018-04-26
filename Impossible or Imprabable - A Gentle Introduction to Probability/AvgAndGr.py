2fb2b952ee8a6888f18a7ba818553c20028d053d
Naman Jain <24namanjain@gmail.com>
2018-04-27 02:58:42 +0530

    Redone

--- Impossible or Imprabable - A Gentle Introduction to Probability/AvgAndGr.py
+++ Impossible or Imprabable - A Gentle Introduction to Probability/AvgAndGr.py
@@ -14,30 +14,36 @@ import pandas as pd
 
 from matplotlib import pyplot as plt
 
-if __name__ == '__main__':
+
+def main():
+    """
+        Main function
+    """
     # the 'result' will hold mean of 1, 2, 3, 4, ... 9999 numbers
     result = list()
 
-    for n in range(1, 10000):
+    for size in range(1, 10000):
         # choose n numbers from 1 to 10
-        nums = np.random.randint(low=1, high=10, size=n)
-
+        nums = np.random.randint(low=1, high=10, size=size)
         # calculating mean of 1, 2, 3, 4 ... 9999 random numbers and adding to the list
-        mean = nums.mean()
-        result.append(mean)
+        result.append(nums.mean())
 
-    df = pd.DataFrame({'means': result})
+    data_frame = pd.DataFrame({'means': result})
 
     # the average in the beginning are all over the place
-    print(df.head())
+    print(data_frame.head())
 
     # as size of data increases the average get close to 5
-    print(df.tail())
+    print(data_frame.tail())
 
-    print(df['means'].mean())       # == actual mean (almost)
+    print(data_frame['means'].mean())  # == actual mean (almost)
 
     # plotting the data
-    df.plot(title="Law of Large Numbers")
+    data_frame.plot(title="Law of Large Numbers")
     plt.xlabel("Number of throws in sample")
     plt.ylabel("Average of Sample")
     plt.show()
+
+
+if __name__ == '__main__':
+    main()
