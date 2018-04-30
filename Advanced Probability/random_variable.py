c02882bfe64ad5722b1febae5104ef0f40572970
Naman Jain <24namanjain@gmail.com>
2018-05-01 01:53:08 +0530

    Random Variable: Started

---
+++ Advanced Probability/random_variable.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+    Data-Science: Created by Naman Jain on 01-05-2018
+    File: random_variable
+
+"""
+
+
+def main():
+    """
+        Main Function
+    """
+    return
+
+
+if __name__ == '__main__':
+    main()
