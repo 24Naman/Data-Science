7bd6018294adf084cd4a1246fc3d58561621d683
naman <24namanjain@gmail.com>
2018-05-26 02:14:33 +0530

    Updated to PEP8 Guidelines

--- Types Of Data/geo_mean.py
+++ Types Of Data/geo_mean.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
 
 """
-    Geometric Mean
+    Geometric Mean:
 
 """
 from functools import reduce
 
 
-if __name__ == '__main__':
+def main():
+    """
+        Main Function
+    :return:
+    """
     # data of temperature
     data = [31, 32, 32, 31, 28, 29, 31, 38, 32, 31, 30, 29, 30, 31, 26]
 
@@ -16,3 +20,7 @@ if __name__ == '__main__':
     geo_mean = product ** (1 / len(data))
 
     print("Geometric Mean = ", geo_mean)
+
+
+if __name__ == '__main__':
+    main()
