1cd230846c9935843b154c651391c91693a02f84
naman <24namanjain@gmail.com>
2018-05-27 00:44:07 +0530

    Geometric Mean Example

--- Types Of Data/geo_mean.py
+++ Types Of Data/geo_mean.py
@@ -1,7 +1,22 @@
 #!/usr/bin/python3
 
 """
-    Geometric Mean:
+    Geometric Mean: The geometric mean is the average growth of an investment computed by
+    multiplying n variables and then taking the n square root.  In other words, it is the average
+    return of an investment over time, a metric used to evaluate the performance of an investment
+    portfolio.
+    **
+    The arithmetic mean is relevant any time several quantities add together to produce a total.
+    The arithmetic mean answers the question, "if all the quantities had the same value, what would
+    that value have to be in order to achieve the same total?"
+    In the same way, the geometric mean is relevant any time several quantities multiply together
+    to produce a product. The geometric mean answers the question, "if all the quantities had the
+    same value, what would that value have to be in order to achieve the same product?"
+    **
+
+    Formula of Geometric Mean = (Product of data) ^ (1 / [len of data])
+
+    !! Product of data = GM ^ (len of data)
 
 """
 from functools import reduce
@@ -12,14 +27,15 @@ def main():
         Main Function
     :return:
     """
-    # data of temperature
-    data = [31, 32, 32, 31, 28, 29, 31, 38, 32, 31, 30, 29, 30, 31, 26]
+    # growth rate in percentage
+    stock_growth_rate = [1, 2, 2, 2, 2, 9, 9, 9, 9]
 
-    product = reduce(lambda x, y: x * y, data)
+    product = reduce(lambda x, y: x * y, stock_growth_rate)
+    geometric_mean = product ** (1 / len(stock_growth_rate))
 
-    geo_mean = product ** (1 / len(data))
-
-    print("Geometric Mean = ", geo_mean)
+    print(f"Geometric Mean: {geometric_mean}")
+    print(f"Calculated Product: {product}")
+    print(f"Original Value: {geometric_mean ** len(stock_growth_rate)}")
 
 
 if __name__ == '__main__':
