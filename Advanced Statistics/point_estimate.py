8bc60e25b2812845aeb8164ad1f7032c511d5635
naman <24namanjain@gmail.com>
2018-06-15 02:40:39 +0530

    Point Estimate

---
+++ Advanced Statistics/point_estimate.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+
+"""
+    04-06-2018
+    Author: Naman Jain
+
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from scipy import stats
+
+
+def main():
+    """
+        Main Function
+    """
+    np.random.seed(1234)
+
+    # rvs generates variable with mean=mu and of size=size
+    # loc is location parameter, it decide the the shift in the distribution
+
+    # # distribution for people who take long break
+    # represents 3000 people who take an average 60 minutes break
+    long_breaks = stats.poisson.rvs(loc=10, mu=60, size=3000)
+
+    pd.Series(long_breaks).hist()
+    # plt.show()
+
+    # # distribution for people who take short break
+    # represents 3000 people who take an average 15 minutes break
+    short_breaks = stats.poisson.rvs(loc=10, mu=15, size=6000)
+
+    pd.Series(short_breaks).hist()
+    # plt.show()
+
+    breaks = np.concatenate((long_breaks, short_breaks))
+
+    pd.Series(breaks).hist()
+    # plt.show()
+
+    # since asking each employee would be very difficult so will make a point estimate
+    # taking sample of 100 people
+    sample_breaks = np.random.choice(a=breaks, size=100)
+    print(breaks.mean() - sample_breaks.mean())
+
+    pd.DataFrame(breaks).hist(bins=50, range=(5, 100))
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
